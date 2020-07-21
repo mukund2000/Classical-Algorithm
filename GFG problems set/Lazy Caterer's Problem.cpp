560371542c9aaf7b9810d2b61449236e84341b58
mukund2000 <mukundrastogixyz@gmail.com>
2020-07-21 17:27:05 +0530

    updated

---
+++ GFG problems set/Lazy Caterer's Problem.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        long long n;
+        cin>>n;
+        cout<<(n*(n+1))/2+1<<"\n";
+    }
+	//code
+	return 0;
+}
